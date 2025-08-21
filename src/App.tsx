29c55e0a643c8be27c84b75becb29251a3fdec8b
Hridoy Mnd <hridoymnd10@gmail.com>
2025-08-21 20:23:34 +0600

    basic file and folder stucture add

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h2>This is App Component</h2>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default App;
